fb2fc1b88c90d35fa26cdc377e6df5063513f9a5
ikaub <dimanovikov22@gmail.com>
2020-10-02 19:31:15 +0300

    Updates

---
+++ precache-manifest.4e2ecfc15fbbacbdfbf9218846b90aa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4966008b4485b95c4f535ffa478c017f",
+    "url": "/test-forms/index.html"
+  },
+  {
+    "revision": "145a381e4715fccb333e",
+    "url": "/test-forms/static/css/main.7e637412.chunk.css"
+  },
+  {
+    "revision": "835754aa88b20a535bc7",
+    "url": "/test-forms/static/js/2.97a3d88d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-forms/static/js/2.97a3d88d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "145a381e4715fccb333e",
+    "url": "/test-forms/static/js/main.e94f8b77.chunk.js"
+  },
+  {
+    "revision": "9731d62cb28be0c2dedc",
+    "url": "/test-forms/static/js/runtime-main.7caf7ed5.js"
+  }
+]);
